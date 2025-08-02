e362fee90bea5e7d0d6a56f4c3ba1e64b290b98d
himath <himathcode100@gmail.com>
2025-08-02 20:12:44 +0530

    feat: add dashboard

--- app/page.js
+++ app/page.js
@@ -1,7 +1,28 @@
-import Image from "next/image";
-
 export default function Home() {
+
+
   return (
-    <h1>hello</h1>
+    <div className="h-screen">
+          <h1 className="text-4xl text-black font-light text-center m-10">Flashcard Quiz App</h1>
+
+        <div className="flex justify-center gap-x-5">
+          <button className="border-2 border-black bg-white text-black hover:bg-black hover:text-white font-medium px-8 py-3 rounded-md transition-all duration-200">Create New Deck</button>
+          <button className="border-2 border-black bg-white text-black hover:bg-black hover:text-white font-medium px-8 py-3 rounded-md transition-all duration-200">Start Quiz</button>
+          
+        </div>
+        <div>
+          your flashcard decks
+        </div>
+        <div>
+          <div>
+            <h2>math basic</h2>
+            <button>edit</button>
+            <button>delete</button>
+          </div>
+          <div>
+            <p>25 cards</p>
+          </div>
+        </div>
+      </div>
   );
 }
