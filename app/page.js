f7736694a44578242b8e9694a57fa2d5c0f92ab4
himath <himathcode100@gmail.com>
2025-08-04 10:04:09 +0530

    feat: initilize dashboard

--- app/page.js
+++ app/page.js
@@ -1,28 +1,52 @@
+import { Edit, Trash2, Plus, Play, Delete } from "lucide-react"
+
+
 export default function Home() {
 
 
   return (
+
     <div className="h-screen">
-          <h1 className="text-4xl text-black font-light text-center m-10">Flashcard Quiz App</h1>
+        <h1 className="text-4xl text-black font-light text-center m-10">Flashcard Quiz App</h1>
 
         <div className="flex justify-center gap-x-5">
-          <button className="border-2 border-black bg-white text-black hover:bg-black hover:text-white font-medium px-8 py-3 rounded-md transition-all duration-200">Create New Deck</button>
-          <button className="border-2 border-black bg-white text-black hover:bg-black hover:text-white font-medium px-8 py-3 rounded-md transition-all duration-200">Start Quiz</button>
-          
+          <button className="border-2 border-black bg-white text-black hover:bg-black hover:text-white font-medium pr-6 pl-4 py-3 rounded-lg transition-all duration-200 cursor-pointer flex gap-x-3">
+            <Plus></Plus>
+            Create New Deck
+          </button>
+          <button className="border-2 border-black bg-black text-white hover:bg-white hover:text-black font-medium px-8 py-3 rounded-md transition-all duration-200 cursor-pointer flex gap-x-5">
+            <Play></Play>
+            Start Quiz
+          </button>
         </div>
-        <div>
-          your flashcard decks
+
+        <div className="flex flex-col mx-80 my-20">
+            <h1 className="text-2xl text-black font-light">Your Flashcard Decks</h1>
+            <hr className="border-gray-200 my-5"></hr>
+            <div className="border border-gray-200 shadow-sm rounded-lg p-6">
+              <div className="flex justify-between">
+                <div>
+                  <h2 className="text-xl font-medium text-black mb-1">Math Basic</h2>
+                </div>
+                <div className="flex gap-x-3">
+                  <button className="text-gray-500 text-sm flex font-medium hover:text-blue-500 hover:cursor-pointer items-center gap-x-1">
+                    <Edit size={20}></Edit>
+                    Edit
+                  </button>
+                  <button className="text-gray-500 text-sm flex font-medium hover:text-red-500 hover:cursor-pointer items-center gap-x-1">
+                    <Delete size={20}></Delete>
+                    Delete
+                  </button>
+                </div>
+              </div>
+              <div>
+                <p className="text-gray-500 text-sm">25 cards</p>
+              </div>
+            </div>
         </div>
-        <div>
-          <div>
-            <h2>math basic</h2>
-            <button>edit</button>
-            <button>delete</button>
-          </div>
-          <div>
-            <p>25 cards</p>
-          </div>
-        </div>
-      </div>
+    </div>
+
+
+
   );
 }
