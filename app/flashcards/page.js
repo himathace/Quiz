c06896bd9255dd415284068539eddab10a933fa1
himath <himathcode100@gmail.com>
2025-08-07 22:19:14 +0530

    feat: add nextjs build in api routes

--- app/flashcards/page.js
+++ app/flashcards/page.js
@@ -2,24 +2,24 @@ export default function Flashcard(){
 
     return(
 
-        <div className="flex flex-col justify-center items-center h-screen">
-            <div>
-                <div className="flex gap-x-40 m-5">
+        <div className="flex flex-col items-center h-screen">
+            <div className="w-2xl">
+                <div className="flex justify-between my-10">
                     <p className="text-gray-600">Back</p>
                     <p className="text-lg font-medium">Deck: Math Basic</p>
                 </div>
                 <div >
-                    <button className="w-full text-center font-medium text-white bg-black py-3 px-4 rounded-lg mb-8">Add New Flashcard</button>
+                    <button className="w-full text-center border-1 border-dashed hover:border-2 cursor-pointer bg-gray-200 font-medium text-black  py-3 px-4 rounded-lg mb-8">Add New Flashcard</button>
                 </div>
                 <div>
                     <p className="text-lg font-medium mb-3">Flashcards:</p>
                 </div>
-                <div className="border border-gray-200 rounded-lg hover:border-gray-300 p-4 mb-10">
+                <div className="border border-gray-200 rounded-lg hover:border-gray-400 p-4 mb-10">
                     card
                 </div>
                 <div className="flex justify-between">
-                    <button className="bg-black text-white py-3 px-10 rounded-lg font-medium">start quiz</button>
-                    <button className="border border-gray-200 py-3 px-10 rounded-lg font-medium" >view status</button>
+                    <button className="bg-black text-white py-3 px-20 rounded-lg font-medium">start quiz</button>
+                    <button className="border border-gray-200 py-3 px-20 rounded-lg font-medium" >view status</button>
                 </div>
             </div>
         </div>
--- app/page.js
+++ app/page.js
@@ -1,14 +1,29 @@
 import { Edit, Trash2, Plus, Play, Delete } from "lucide-react"
 
+const fetchdata=async ()=>{
 
-export default function Home() {
+  try{
 
+    const userdata=await fetch("http://localhost:3000/api/login")
+    const data=await userdata.json()
+    return data.message
+  }
+  catch(error){
+    console.log(error)
+  }
+}
+
+
+const Home=async ()=>{
+
+  const date=await fetchdata()
+  console.log(date)
 
   return (
-
+  
     <div className="h-screen">
         <h1 className="text-4xl text-black font-light text-center m-10">Flashcard Quiz App</h1>
-
+  
         <div className="flex justify-center gap-x-5">
           <button className="border-2 border-black bg-white text-black hover:bg-black hover:text-white font-medium pr-6 pl-4 py-3 rounded-lg transition-all duration-200 cursor-pointer flex gap-x-3">
             <Plus></Plus>
@@ -19,7 +34,7 @@ export default function Home() {
             Start Quiz
           </button>
         </div>
-
+  
         <div className="flex flex-col mx-80 my-20">
             <h1 className="text-2xl text-black font-light">Your Flashcard Decks</h1>
             <hr className="border-gray-200 my-5"></hr>
@@ -45,8 +60,10 @@ export default function Home() {
             </div>
         </div>
     </div>
-
-
-
+  
+  
+  
   );
 }
+  
+export default Home
